af2917b24bc1cc28ebefcf18a37f34e700843796
ozgesavo <161982657+ozgesavo@users.noreply.github.com>
2025-04-09 19:56:49 -0400

    Update NotFound.js

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,1 +1,12 @@
-src/pages/NotFound.js
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div style={{ padding: '3rem', textAlign: 'center' }}>
+      <h1>404 - Page Not Found</h1>
+    </div>
+  );
+};
+
+export default NotFound;
+
