98b5ecb0c2076eb450d7d47a8a220af6992696c7
ozgesavo <161982657+ozgesavo@users.noreply.github.com>
2025-04-09 20:22:15 -0400

    Create SupplierDocuments.js

---
+++ src/pages/SupplierDocuments.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SupplierDocuments = () => {
+  return (
+    <div style={{ padding: '3rem', textAlign: 'center' }}>
+      <h1>Supplier Documents Page</h1>
+    </div>
+  );
+};
+
+export default SupplierDocuments;
