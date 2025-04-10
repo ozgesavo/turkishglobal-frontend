cd36991d8a29e007824b9349dc0946959851d3bb
ozgesavo <161982657+ozgesavo@users.noreply.github.com>
2025-04-09 20:11:58 -0400

    Update SupplierProfile.js

--- src/pages/SupplierProfile.js
+++ src/pages/SupplierProfile.js
@@ -1,1 +1,12 @@
+import React from 'react';
+
+const SupplierProfile = () => {
+  return (
+    <div style={{ padding: '3rem', textAlign: 'center' }}>
+      <h1>Supplier Profile Page</h1>
+    </div>
+  );
+};
+
+export default SupplierProfile;
 
